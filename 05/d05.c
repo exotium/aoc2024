e24ac753d35b8629d88f7265d596024a5afc3e7c
exotium <hanna.biermans1@outlook.com>
2024-12-05 17:51:57 +0100

    day 5 - the return of qsort

---
+++ 05/d05.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+#define B 15201
+
+A,o,c,D,a,y,V[3],_[99],$[100][100];C(void*a,void*
+b){o|=c=$[*(int*)b][*(int*)a];return c-1;}main(){
+char*h,*i=mmap(0,B,1,1,open("input.txt",0),0);for
+(h=i+B;*i^10;D=strtol(i,&i,10),a=strtol(i+1,&i,10
+),$[D][a]=2,i++);for(;++i<h;_[y++]=strtol(i,&i,10
+),*i==10?qsort(_,y,4,C),V[o]+=_[y/2],o=y=0:0);
+printf("%d\n%d",*V,V[2]);}
